c57e7e165087736a55edc7d82b0205e9a773c44b
Down709 <adhamel3ashry@gmail.com>
2025-10-26 14:38:09 +0300

    fix and formate

--- JavaApplication8/src/javaapplication8/Database.java
+++ JavaApplication8/src/javaapplication8/Database.java
@@ -13,11 +13,11 @@ import javax.swing.JOptionPane;
  * @author Khaled
  */
 public class Database {
+
     private static final String FILE_NAME = "students.txt";
 
     public static void saveToFile(List<Student> students) {
         try (PrintWriter pw = new PrintWriter(new FileWriter(FILE_NAME))) {
-            // traditional for loop instead of for-each
             for (int i = 0; i < students.size(); i++) {
                 Student s = students.get(i);
                 pw.println(s.getId() + "," + s.getName() + "," + s.getAge() + "," + s.getGender() + "," + s.getDepartment() + "," + s.getGpa());
@@ -29,13 +29,12 @@ public class Database {
             JOptionPane.showMessageDialog(null, "Error saving student data: " + e.getMessage(), "File Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
     public static List<Student> loadFromFile() {
         List<Student> students = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
             String line;
-
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
                 if (data.length == 6) {
@@ -49,7 +48,6 @@ public class Database {
                     students.add(new Student(id, name, age, gender, department, gpa));
                 }
             }
-
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, "No existing student file found,a new one will be created", "Info", JOptionPane.INFORMATION_MESSAGE);
 
